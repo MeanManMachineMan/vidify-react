23d9fb23bb77cfb4e2821eda555ec4c195a2e95f
Vidur <vidur.p11@gmail.com>
2021-12-10 23:11:36 +0530

    Initialized Vidify! :)

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import 'tailwindcss/tailwind.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className="p-3">
+      Welcome to Vidify!
+    </div>
+  )
+}
+
+export default Home
